e87e70375b6f02966a15cef37de89e1563e6d46d
Daniel Melody <dmwalter12@gmail.com>
2019-05-30 14:37:14 -0400

    command line game complete

--- connect4.py
+++ connect4.py
@@ -42,8 +42,20 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r+1][c] == piece
             and board[r+2][c] == piece and board[r+3][c] == piece:
                 return True
-    # Check diagonals
 
+    # Check positive-slope diagonals
+    for r in range(COLUMN_COUNT-3):
+        for c in range(ROW_COUNT):
+            if board[r][c] == piece and board[r+1][c+1] == piece
+            and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    # Check negative-slope diagonals
+    for r in range(ROW_COUNT-3):
+        for c in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece
+            and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
 
 board = create_board()
 print_board(board)
@@ -66,11 +78,15 @@ while not game_over:
     # ask for player 2 input
     else:
         col = int(input("Player 2 make your selection (0-6): "))
-
+        # Check valid piece placement
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
-
+        # Check for winning move
+        if winning_move(board, 2):
+            print("Player 2 Wins!")
+            game_over = True     # Game is over
+            
         print_board(board)
     turn += 1
     turn = turn % 2
